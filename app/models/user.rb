785870963b49ea6e5e4d01de07c6084ab31ca330
czarjulius <juliusczar.jc@gmail.com>
2020-04-25 15:27:08 +0200

    implemented the signup feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :user_type, presence: true
 end
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "User", type: :request do
 
   describe "GET /index" do
     it "returns http success" do
       get "/user/index"
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  context 'user_signup' do
+    it 'creates a new user' do
+      headers = { 'ACCEPT' => 'application/json' }
+      post '/signup', params: { user: { first_name: 'Julius', last_name: 'Ngwu', email:'julius@1', password:'julius@@1', user_type:'mentee' } }, headers: headers
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      expect(response).to have_http_status(:created)
+    end
+    it 'fails to creates a user' do
+      headers = { 'ACCEPT' => 'application/json' }
+      post '/signup', params: { user: { firkst_name: 'Julius', last_name: 'Ngwu', email:'julius@1', password:'julius@@1', user_type:'mentee' } }, headers: headers
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      expect(response.body).to include('error')
     end
   end
 
