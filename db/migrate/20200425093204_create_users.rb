724ad39741a13d8f1913ed3e05130690a9fcd643
czarjulius <juliusczar.jc@gmail.com>
2020-04-25 12:15:07 +0200

    integrated travis

--- db/migrate/20200425093204_create_users.rb
+++ db/migrate/20200425093204_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name, null: false
       t.string :email, null: false
       t.string :password, null: false
-      t.string :type, null: false
+      t.string :user_type, null: false
 
       t.timestamps null: false
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid when all attributes are provided' do
+    new_user = User.new(first_name: 'Julius', last_name: 'Ngwu', email:'julius@gmail.com',password:'julius@1', user_type:'mentee')
+    expect(new_user).to be_valid
+  end
 end
