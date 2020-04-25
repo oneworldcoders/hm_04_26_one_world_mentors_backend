27777e5867e08f5ac404f79d919769ef9f51583b
Mihigo Rugamba <robert.rugamba3@hotmail.com>
2020-04-25 13:57:17 +0200

    adds endpoint for returning list of mentors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'user#index'
+  get 'user/index'
   post 'signup', to: 'user#signup'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'mentors', to: 'mentors#mentors'
 end
---
+++ spec/requests/mentor_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+
+    describe "GET /mentors" do
+        it "returns empty list of mentors" do
+          get "/mentors"
+          expect(response).to have_http_status(:success)
+        end
+
+        it "returns a single mentor in a list" do
+          user = User.new(first_name: "mike", last_name: "", email:"", password: "", user_type:"mentor")
+          user.save
+          get "/mentors"
+          expect(response.body).to include("mike")
+
+        end
+    end
+end
