98598df643c4c74b0123cef85e84be13ceab347f
Mihigo Rugamba <robert.rugamba3@hotmail.com>
2020-04-25 17:36:23 +0200

    adds more test coverage

--- spec/requests/mentor_request_spec.rb
+++ spec/requests/mentor_request_spec.rb
@@ -9,10 +9,15 @@ RSpec.describe "Users", type: :request do
         end
 
         it "returns a single mentor in a list" do
-          user = User.new(first_name: "mike", last_name: "", email:"", password: "", user_type:"mentor")
-          user.save
+          user = User.create(first_name: "mike", last_name: "tyson", email:"rm@mail.com", password: "yes", user_type:"mentor")
           get "/mentors"
           expect(response.body).to include("mike")
+          mentors = JSON.parse(response.body)
+          expect(mentors["mentors"].first["first_name"]).to eq("mike")
+          expect(mentors["mentors"].first["last_name"]).to eq("tyson")
+          expect(mentors["mentors"].first["email"]).to eq("rm@mail.com")
+          expect(mentors["mentors"].first["password"]).to eq("yes")
+          expect(mentors["mentors"].first["user_type"]).to eq("mentor")
         end
     end
 end
