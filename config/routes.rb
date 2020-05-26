4f051d10119f4069d57f3dc041d3ba178f23faa8
czarjulius <juliusczar.jc@gmail.com>
2020-05-26 11:14:32 +0200

    create a new course

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  
-  root 'user#index'
-  get 'user/index'
-  post 'signup', to: 'user#signup'
-  patch 'user/profile/:id', to: 'user#update_user'
-  get 'mentors', to: 'mentors#mentors'
-  get 'courses', to: 'courses#courses'
+  resources :courses
+
+  root "user#index"
+  get "user/index"
+  post "signup", to: "user#signup"
+  patch "user/profile/:id", to: "user#update_user"
+  get "mentors", to: "mentors#mentors"
   post "login" => "auth#create"
-  get 'profile', to: 'profile#show'
+  get "profile", to: "profile#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
