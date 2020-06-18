625fff0bdb1447d230e92925b3e14c2dfe3cb982
czarjulius <juliusczar.jc@gmail.com>
2020-06-18 18:39:52 +0200

    mentee add course

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
-  factory :user, class: User do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Internet.email }
-    password { Faker::Crypto.md5 }
-    user_type { Faker::Name.name }
-  end
+    factory :user, class: User do
+        first_name { Faker::Name.first_name }
+        last_name { Faker::Name.last_name }
+        email { Faker::Internet.email }
+        password {Helpers::TEST_PASSWORD}
+        user_type { Faker::Name.name }
+    end
 
   factory :course, class: Course do
     courseCode { Faker::Code.asin }
