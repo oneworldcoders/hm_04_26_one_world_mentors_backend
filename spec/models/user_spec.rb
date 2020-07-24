f607f8a4c9a0d21c72463225a6d3e9b0ee2f1a12
Emmanuel Omona <emmanuelomona1@gmail.com>
2020-07-24 17:10:34 +0200

    populate mentor and mentee table on user sigup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,11 +11,31 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
   it"The authenticate method should return  valid user if credentials are valid" do
-  auth_user=User.authenticate(user["email"],Helpers::TEST_PASSWORD)
-  expect(auth_user["email"]).to eq(user["email"])
-end
-it"The authenticate method should returna nil if credentials are not valid" do
-  auth_user=User.authenticate("joy123","williams") 
-  expect(auth_user).to eq(nil)
-end
+    auth_user=User.authenticate(user["email"],Helpers::TEST_PASSWORD)
+    expect(auth_user["email"]).to eq(user["email"])
+  end
+  it"The authenticate method should returna nil if credentials are not valid" do
+    auth_user=User.authenticate("joy123","williams") 
+    expect(auth_user).to eq(nil)
+  end
+
+  context 'after signup' do
+    before do
+      user = FactoryBot.create(:user, user_type: 'mentor')
+      @mentor = Mentor.find_by(user_id: user.id)
+    end
+
+    it "creates a mentor on user signup" do
+      expect(@mentor).to be_truthy
+    end
+
+    it "creates a mentor on user signup" do
+      expect(@mentor.available).to eq true
+    end
+
+    it "creates a mentee on user signup" do
+      user = FactoryBot.create(:user, user_type: 'mentee')
+      expect(Mentee.find_by(user_id: user.id)).to be_truthy
+    end
+  end
 end
